0ebaebfb29141b0a128016f98c429d46f1d04799
hk-jessliu <ff9783aa@gmail.com>
2020-11-24 16:13:59 +0800

    swimage.sh

---
+++ trimage.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -x
+#set -e
+
+init(){
+       
+	soure_path=/mnt/Engineering/Project_Management/General/TechnicalData/TestCaseDocument/TestPlan/Link_Abstraction/images
+        my_path=/mnt/Public/Jess/image/traffic-image
+
+}
+
+move_image(){
+                       
+        sudo mv ${my_path}/image.bin.e2900*.LA ${my_path}/2900old 
+        sudo mv ${my_path}/image.bin.e6000*.LA ${my_path}/6000old 	
+	
+	if [ -f "${my_path}"/image*.LA ];then
+	   
+		echo "mv old image fail"
+        else 
+            
+		echo "mv old image finish"		    
+    
+        fi
+
+
+	sudo cp ${soure_path}/image*.LA ${my_path} 
+   
+       	if [ -f "${my_path}"/image.bin.e2900*.LA ];then
+	  
+	        echo "2900 image update ok"
+        
+        fi
+     
+        if [ -f "${my_path}"/image.bin.e6000*.LA ];then
+	  
+	      	echo "6000 image update ok"
+	fi
+
+}
+
+check_image(){
+
+	before=$(date -d '3 days ago' "+%m%d")
+        before1=$(date -d '1 days ago' "+%m%d")
+	
+       # image_file1=${soure_path}/image.bin.e2900.ewn.${before}"10".id-*.LA
+       # image_file2=${soure_path}/image.bin.e2900.ewn.${before1}"10".id-*.LA
+       # image_file3=${soure_path}/image.bin.e6000.ewn.${before}"10".id-*.LA
+       # image_file4=${soure_path}/image.bin.e6000.ewn.${before1}"10".id-*.LA
+	
+	
+	if [ -f "${soure_path}"/image.bin.e2900.ewn.${before}"10".id-*.LA ];then
+
+      		sudo mv ${soure_path}/image.bin.e2900.ewn.${before}"10".id-*.LA ${soure_path}/old && echo "mv old 2900image finish"
+
+		sudo mv ${soure_path}/image.bin.e6000.ewn.${before}"10".id-*.LA ${soure_path}/old && echo "mv old 6000image finish"
+	fi
+
+	if [ -f "${soure_path}"/image.bin.e2900.ewn.${before1}"10".id-*.LA ];then
+
+
+		sudo mv ${soure_path}/image.bin.e2900.ewn.${before1}"10".id-*.LA ${soure_path}/old && echo "mv old 2900image finish"
+
+		sudo mv ${soure_path}/image.bin.e6000.ewn.${before1}"10".id-*.LA ${soure_path}/old && echo "mv old 6000image finish"
+	fi
+
+
+}
+
+
+init
+check_image
+move_image
---
+++ trsetup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -x   
+
+init(){
+      
+	Dev_path=/dev/shm
+        Home_path=${HOME}      
+        Share_path=/mnt/Engineering/Project_Management/General/TechnicalData/TestCaseDocument/TestPlan/Link_Abstraction/traffic
+}
+
+remove_tar(){
+   
+       cd ${Home_path}
+       sudo rm -rf traffic.tgz
+       cd ${Dev_path}
+       sudo rm -rf traffic*      
+
+}
+
+cp_traffic(){
+
+       sudo  cp ${Share_path}/traffic*.tar /dev/shm 
+
+}
+
+create_tar_file(){
+        
+        cd ${Dev_path}
+        traffic_file=traffic*.tar        	
+        tar xvf ${traffic_file}
+}
+
+use_traffic(){
+        
+	cd ${Dev_path}
+        cd traffic
+	demo/run-aws.sh setup
+	demo/run-aws.sh createpkg
+}
+
+put_server(){
+           
+        cd ${Home_path}
+        sudo scp ${Home_path}/traffic.tgz ubuntu@34.247.164.77:/home/ubuntu	
+
+}
+
+
+init
+remove_tar
+cp_traffic
+create_tar_file
+use_traffic
+put_server 
